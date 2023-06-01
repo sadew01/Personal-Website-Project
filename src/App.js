9a1a0192001ace882590398d255beed697210fee
Sade Whitcomb <swhitcomb@ufl.edu>
2023-05-31 22:59:14 -0400

    fixed url path

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ function App() {
       <Router>
         <Navbar/>
         <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/projects" element={<Projects/>}></Route>
-          <Route path="/experiences" element={<Experiences/>}></Route>
-          <Route path="/displayproject/:id" element={<DisplayProject/>}></Route>
+          <Route path="/Personal-Website-Project/" element={<Home/>}></Route>
+          <Route path="/Personal-Website-Project/projects" element={<Projects/>}></Route>
+          <Route path="/Personal-Website-Project/experiences" element={<Experiences/>}></Route>
+          <Route path="/Personal-Website-Project/displayproject/:id" element={<DisplayProject/>}></Route>
         </Routes>
         <Footer/>
       </Router>
